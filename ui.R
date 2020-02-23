36d1e9225ff52d65dcbb4c28912970efb22efff3
PAYNE <Jason.Payne@woodgroup.com>
2020-02-23 20:14:41 +0000

    Add hyperlinks to resources

--- ui.R
+++ ui.R
@@ -6,13 +6,13 @@ names(projects)[1] <- "Name"
 
 penPicture <- "I'm currently Head of Data Science for Wood and Analytics Manager for Wood Ireland.  
 I have a Bachelor of Science degree and Diploma in Structural Engineering from Technology University Dublin. 
-I have over 20 years experience in delivery of engineering, technology and data projects.  In the last 5 years, 
+I have over 20 years experience in delivery of engineering, digital and technology projects.  In the last 5 years, 
 I have specialized in data science, managing Wood's centre of excellence for data analytics (located in Ireland) 
 and delivering a wide range of AI & analytics projects across multiple industries and business functions."
 
 myCurrentRole <- "Data Science Manager"
 
-trainingDescription <- "Training courses in Data-driven techniques, tools & strategies
+trainingDescription <- "On-site training courses in data-driven techniques, tools & strategies
 delivered to teams sizes up to 15 people over 1-2 days designed to transform working practices."
 
 header <- dashboardHeader(title = "www.ORB10X.com", titleWidth = 220)
@@ -78,39 +78,57 @@ body <- dashboardBody(
           div("Favourite Resources", class = "body-heading"),
           div(
             div(tags$i(class = "fal fa-book fa-fw"), class = "contact-icon  slider-middle"), 
-            div(span("Book: ", style = "color:#EBCA48;"), "www.machinelearningbook.com", class = "contact-text  slider-middle")
+            div(span("Book: ", style = "color:#EBCA48;"), 
+                a("www.machinelearningbook.com", href="https://www.machinelearningbook.com/", target="_blank"), 
+                class = "contact-text  slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-file-alt fa-fw"), class = "contact-icon  slider-middle"), 
-            div(span("Newsletter: ", style = "color:#EBCA48;"), "www.dataelixir.com", class = "contact-text  slider-middle")
+            div(span("Newsletter: ", style = "color:#EBCA48;"), 
+                a("Data Elixir", href="https://www.dataelixir.com/", target="_blank"), 
+                class = "contact-text  slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-file-alt fa-fw"), class = "contact-icon  slider-middle"), 
-            div(span("Papers: ", style = "color:#EBCA48;"), "www.paperswithcode.com", class = "contact-text  slider-middle")
+            div(span("Papers: ", style = "color:#EBCA48;"), 
+                a("Papers with Code", href="https://www.paperswithcode.com/", target="_blank"), 
+                class = "contact-text  slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-file-audio fa-fw"), class = "contact-icon slider-middle" ), 
-            div(span("Audiobook: ", style = "color:#EBCA48;"), "Data Science, J. Kelleher & B. Tierney", class = "contact-text slider-middle")
+            div(span("Audiobook: ", style = "color:#EBCA48;"), 
+                a("Data Science, J. Kelleher & B. Tierney", href="https://www.amazon.com/Data-Science/dp/B07CB26GZC/", target="_blank"),
+                class = "contact-text slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-podcast fa-fw"), class = "contact-icon slider-middle" ), 
-            div(span("Podcast: ", style = "color:#EBCA48;"), "Data Stories", class = "contact-text slider-middle")
+            div(span("Podcast: ", style = "color:#EBCA48;"), 
+                a("Data Stories", href="https://www.datastori.es/", target="_blank"), 
+                class = "contact-text slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-podcast fa-fw"), class = "contact-icon slider-middle" ), 
-            div(span("Podcast: ", style = "color:#EBCA48;"), "Linear Digressions", class = "contact-text slider-middle")
+            div(span("Podcast: ", style = "color:#EBCA48;"), 
+                a("Linear Digressions", href="https://www.lineardigressions.com/", target="_blank"), 
+                class = "contact-text slider-middle")
           ), 
           div(
             div(tags$i(class = "fal fa-podcast fa-fw"), class = "contact-icon slider-middle" ), 
-            div(span("Podcast: ", style = "color:#EBCA48;"), "Talking Machines", class = "contact-text slider-middle")
+            div(span("Podcast: ", style = "color:#EBCA48;"), 
+                a("Talking Machines", href="https://www.thetalkingmachines.com/", target="_blank"), 
+                class = "contact-text slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-chalkboard-teacher fa-fw"), class = "contact-icon slider-middle" ), 
-            div(span("MOOC: ", style = "color:#EBCA48;"), "Analytics Edge, edX / MITx", class = "contact-text slider-middle")
+            div(span("MOOC: ", style = "color:#EBCA48;"), 
+                a("Analytics Edge, edX / MITx", href="https://www.edx.org/course/the-analytics-edge/", target="_blank"), 
+                class = "contact-text slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-chalkboard-teacher fa-fw"), class = "contact-icon slider-middle" ), 
-            div(span("Tutorials: ", style = "color:#EBCA48;"), "www.datacamp.com", class = "contact-text slider-middle")
+            div(span("Tutorials: ", style = "color:#EBCA48;"), 
+                a("Data Camp", href="https://www.datacamp.com/", target="_blank"), 
+                class = "contact-text slider-middle")
           ), style = "margin:15px;"
         ),
         br(),
@@ -140,6 +158,10 @@ body <- dashboardBody(
             div("Engineering, Mathematics & Problem Solving", class = "contact-text slider-middle")
           ),
           div(
+            div(tags$i(class = "fal fa-trophy-alt fa-fw"), class = "contact-icon slider-middle" ), 
+            div("Scoping & Winning New work", class = "contact-text slider-middle")
+          ),
+          div(
             div(tags$i(class = "fal fa-pencil-ruler fa-fw"), class = "contact-icon slider-middle" ), 
             div("Graphic Design (Slides, Infographics & UIs)", class = "contact-text slider-middle")
           ), style = "margin:15px;"
@@ -149,7 +171,7 @@ body <- dashboardBody(
         # Add a section on personal interests                                  |
         # ---------------------------------------------------------------------+
         div(
-          div("Interests", class = "body-heading"),
+          div("Passions", class = "body-heading"),
           div(
             div(tags$i(class = "fas fa-running fa-fw"), class = "contact-icon  slider-middle"), 
             div("Running trails, hills & mountains", class = "contact-text  slider-middle")
@@ -160,11 +182,11 @@ body <- dashboardBody(
           ),
           div(
             div(tags$i(class = "far fa-film fa-fw"), class = "contact-icon slider-middle" ), 
-            div("Movies, documentaries & boxsets", class = "contact-text slider-middle")
+            div("Movies, documentaries & series", class = "contact-text slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-dog-leashed fa-fw"), class = "contact-icon slider-middle" ), 
-            div("Walking my dog in the woods", class = "contact-text slider-middle")
+            div("Walking my dog", class = "contact-text slider-middle")
           ),
           div(
             div(tags$i(class = "fal fa-child fa-fw"), class = "contact-icon slider-middle" ), 
